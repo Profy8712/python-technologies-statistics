a86697d674b2959eac9887d2dd0f4d16c4bcfea9
Oleksandr Kurin <profysss8712@gmail.com>
2025-06-25 18:00:28 +0300

    Run experience-level analysis after main analysis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from scraping.work_ua_parser import fetch_vacancies
-from analysis.analyzer import run_analysis
+from analysis.analyzer import run_analysis, run_analysis_by_levels
 
 if __name__ == "__main__":
     fetch_vacancies()
     run_analysis()
+    run_analysis_by_levels()
